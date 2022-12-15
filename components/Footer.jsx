7a4e6626e3b790489404fc9856613a8aa995b71d
DiogoBatista1 <diogobatista330@gmail.com>
2022-12-15 05:26:37 -0300

    Update

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+// import Image from "next/image";
+// import styles from "../styles/Footer.module.css";
+
+const Footer = () => {
+    return (
+        <div className="styles.container"></div>
+    )
+}
+
+export default Footer;
